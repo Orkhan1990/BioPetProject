569059d64c635d7d5bf89d75a217864c79b03d81
Orkhan Talibli <orkhan.talibli@gmail.com>
2023-02-05 00:41:49 +0400

    fifth commit

---
+++ HomePage/home.js
@@ -0,0 +1,16 @@
+let navbarList=document.querySelectorAll('.fist_list_droptown');
+let subList=document.querySelectorAll('.sub_list');
+console.log(navbarList);
+
+
+
+// for(let i=0;i<navbarList.length;i++){
+    
+//     navbarList[i].addEventListener('click',()=>{
+//         for(let i=0;i<subList.length;i++){
+//             subList[i].classList.toggle('active_list');
+//         }
+
+
+//     })
+// }
